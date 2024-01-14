b78fcf700cc8c17eb4ab832606dee917588a4ce8
David <dsolarte2809@gmail.com>
2024-01-14 00:08:12 -0500

    Arreglo Models

--- udenews/models.py
+++ udenews/models.py
@@ -3,9 +3,9 @@ from django.urls import reverse
 # Create your models here.
 class News(models.Model):
     title = models.CharField(max_length=100)
-    img   = models.ImageField(upload_to='news/', blank=True,)
-    date  = models.DateField(blank=True)
-    place = models.CharField(max_length=100, blank=True)
+    img   = models.ImageField(upload_to='news/', blank=True, null=True)
+    date  = models.DateField(blank=True, null=True)
+    place = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.title
